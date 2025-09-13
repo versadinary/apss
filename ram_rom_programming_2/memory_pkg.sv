a542010c31ac31f102783bee72bb1531443509f5
versadinary <170194434+versadinary@users.noreply.github.com>
2025-09-13 14:49:19 +0300

    instruction memory & memory package

---
+++ ram_rom_programming_2/memory_pkg.sv
@@ -0,0 +1,8 @@
+package memory_pkg;
+
+  localparam INSTR_MEM_SIZE_BYTES = 512;
+  localparam INSTR_MEM_SIZE_WORDS = INSTR_MEM_SIZE_BYTES / 4;
+  localparam DATA_MEM_SIZE_BYTES  = 512;
+  localparam DATA_MEM_SIZE_WORDS  = DATA_MEM_SIZE_BYTES / 4;
+
+endpackage
