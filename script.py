190ab400cbe6f8aabca4419f75464c6965b5325f
versadinary <170194434+versadinary@users.noreply.github.com>
2025-09-21 23:23:25 +0300

    .

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 def first_operand(num):
-    return hex((num < 0 << 23) + num * 2)[2::]
+    return hex(((num < 0) << 23) + num * 2)[2::]
 
 def second_operand(num):
-    return hex((num < 0 ? << 16) + num)[2::]
+    return hex(((num < 0) << 16) + num)[2::]
 
-a, b = int(input("a: ")), int(input("sw: "))
-print(first_operand(a))
+a, sw = int(input("a: ")), int(input("sw: "))
+print("a:", first_operand(a))
+print("sw:", second_operand(sw))
