00afd841750649a116549f5e99104b31bef1dc59
versadinary <170194434+versadinary@users.noreply.github.com>
2025-09-21 22:48:00 +0300

    script - 2

--- script.py
+++ script.py
@@ -1,12 +1,8 @@
-def twos_complement(num, bits):
-    if num >= 0:
-        return hex(num)[2:]
-    else:
-        return hex((1 << bits) + num)[2:]
+def first_operand(num):
+    return hex((num < 0 << 23) + num * 2)[2::]
 
-
-
+def second_operand(num):
+    return hex((num < 0 ? << 16) + num)[2::]
 
 a, b = int(input("a: ")), int(input("sw: "))
-print(twos_complement(a, 23))
-print(twos_complement(b, 16))
+print(first_operand(a))
