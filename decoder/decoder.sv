846710ff830f13bad057d46d45dcaef5b5e1148a
versadinary <170194434+versadinary@users.noreply.github.com>
2025-09-25 13:33:07 +0300

    always and default signals

--- decoder/decoder.sv
+++ decoder/decoder.sv
@@ -17,7 +17,33 @@ module decoder (
                 output logic       mret_o
                 );
 
+   logic [6:0]                     opcode;
+   logic [2:0]                     func3;
+   logic [6:0]                     func7;
 
+   assign opcode = fetched_instr_i[6:0];
+   assign func3 = fetcher_instr_i[14:12];
+   assign func7 = fetched_instr_i[31:25];
 
+   always @ (*) begin
+      a_sel_o = 2'd0;
+      b_sel_o = 3'd0;
+      alu_op_o = 5'd0;
+      csr_op_o = 3'd0;
+      csr_we_o = 1'd0;
+      mem_req_o = 1'd0;
+      mem_size_o = 3'd0;
+      gpr_we_o = 1'd0;
+      wb_sel_o = 2'd0;
+      branch_o = 1'd0;
+      jal_o = 1'd0;
+      jalr_o = 1'd0;
+      mret_o = 1'd0;
+      illegal_instr_o = &opcode[1:0]; // in next usages dusjunct with this vaule
+      case (opcode[6:2]) begin
+
+      endcase // case (opcode)
+
+   end
 
 endmodule 
