c48667ec89957c8b9e09546bd2e011a39363d63e
versadinary <170194434+versadinary@users.noreply.github.com>
2025-10-05 19:43:57 +0300

    processor core: add wires and ports

--- lsu_5/processor/processor_system.sv
+++ lsu_5/processor/processor_system.sv
@@ -5,13 +5,10 @@ module processor_system(
 
    logic                            stall, stall_tg, mwe, mreq;
    logic [31:0]                     mwd, maddr, mrd, rom_ins, rom_addr;
-
-   assign stall = ~stall_tg & mreq;
-
-   always @ (posedge clk_i) begin
-      if (rst_i) stall_tg <= 1'd0;
-      else stall_tg <= stall;
-   end
+   logic [2:0]                      msize;
+   logic                            dmem_req, dmem_we, dmem_rd, ready;
+   logic [3:0]                      dmem_be;
+   logic [31:0]                     dmem_wd, dmem_a;
 
    processor_core core(
                        .clk_i(clk_i),
@@ -21,29 +18,29 @@ module processor_system(
                        .mem_rd_i(mrd),
                        .instr_addr_o(rom_addr),
                        .mem_addr_o(maddr),
-                       // .mem_size_o(),
+                       .mem_size_o(msize),
                        .mem_req_o(mreq),
                        .mem_we_o(mwe),
                        .mem_wd_o(mwd)
                        );
 
    lsu lsu_main(
-                .clk_i(),
-                .rst_i(),
-                .core_req_i(),
-                .core_we_i(),
-                .core_size_i(),
-                .core_addr_i(),
-                .core_wd_i(),
-                .core_rd_o(),
-                .core_stall_o(),
-                .mem_req_o(),
-                .mem_we_o(),
-                .mem_be_o(),
-                .mem_addr_o(),
-                .mem_wd_o(),
-                .mem_rd_i(),
-                .mem_ready_i()
+                .clk_i(clk_i),
+                .rst_i(rst_i),
+                .core_req_i(mreq),
+                .core_we_i(mwe),
+                .core_size_i(msize),
+                .core_addr_i(maddr),
+                .core_wd_i(mwd),
+                .core_rd_o(mrd),
+                .core_stall_o(stall),
+                .mem_req_o(dmem_req),
+                .mem_we_o(dmem_we),
+                .mem_be_o(dmem_be),
+                .mem_addr_o(dmem_a),
+                .mem_wd_o(dmem_wd),
+                .mem_rd_i(dmem_rd),
+                .mem_ready_i(ready)
                 );
 
    instr_mem imem(
@@ -53,13 +50,13 @@ module processor_system(
 
    data_mem dmem(
                  .clk_i(clk_i),
-                 .mem_req_i(mreq),
-                 .write_enable_i(mwe),
-                 .byte_enable_i(4'b1111),
-                 .addr_i(maddr),
-                 .write_data_i(mwd),
-                 .read_data_o(mrd)
-                 // .ready_o()
+                 .mem_req_i(dmem_req),
+                 .write_enable_i(dmem_we),
+                 .byte_enable_i(dmem_be),
+                 .addr_i(dmem_a),
+                 .write_data_i(dmem_a),
+                 .read_data_o(dmem_rd)
+                 .ready_o(ready)
                  );
 
 endmodule // processor_system
