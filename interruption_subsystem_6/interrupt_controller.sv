a0b8f36694bb3947af82ee23a172af0d9001e0d3
versadinary <170194434+versadinary@users.noreply.github.com>
2025-10-18 23:57:45 +0300

    interrupt controller decl

---
+++ interruption_subsystem_6/interrupt_controller.sv
@@ -0,0 +1,14 @@
+module interrupt_controller(
+                            input logic         clk_i,
+                            input logic         rst_i,
+                            input logic         exception_i,
+                            input logic         irq_req_i,
+                            input logic         mie_i,
+                            input logic         mret_i,
+
+                            output logic        irq_ret_o,
+                            output logic [31:0] irq_cause_o,
+                            output logic        irq_o
+                            );
+
+endmodule
