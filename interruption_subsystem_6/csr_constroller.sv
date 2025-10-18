d0fa453d811e81111d405aa91ab14e8c469e4b5d
versadinary <170194434+versadinary@users.noreply.github.com>
2025-10-18 23:44:05 +0300

    csr controller

--- interruption_subsystem_6/csr_constroller.sv
+++ interruption_subsystem_6/csr_constroller.sv
@@ -20,15 +20,79 @@ module csr_constroller(
 
    import csr_pkg::*;
 
-   logic                                   reg_data;
-   logic                                   csr;
+   logic                                   mie_en;
+   logic                                   mtvec_en;
+   logic                                   mscratch_en;
+   logic                                   mepc_en;
+   logic                                   mcause_en;
+
+   logic [31:0]                            mie_q;
+   logic [31:0]                            mtvec_q;
+   logic [31:0]                            mscratch_q;
+   logic [31:0]                            mepc_q;
+   logic [31:0]                            mcause_q;
+   logic [31:0]                            reg_data;
+   logic [31:0]                            csr;
+
+   assign mie_o = mie_q;
+   assign mtvec_o = mtvec_q;
+   assign mepc_o = mepc_q;
 
    always @ (*) begin
       case (opcode_i)
         CSR_RW: csr = rs1_data_i;
-        CSR_RS: csr
+        CSR_RS: csr = rs1_data_i | csr;
+        CSR_RC: csr = ~rs1_data_i & csr;
+        CSR_RWI: csr = imm_data_i;
+        CSR_RSI: csr = csr | imm_data_i;
+        CSR_RCI: csr = csr & ~imm_data_i;
+        default: csr = '0;
       endcase // case (opcode_i)
    end
 
+   always @ (*) begin
+      {mie_en, mtvec_en, mscratch_en, mepc_en, mcause_en} = 'd0;
+      case (addr_i)
+        MIE_ADDR: mie_en = write_enable_i;
+        MTVEC_ADDR: mtvec_en = write_enable_i;
+        MSCRATCH_ADDR: mscratch_en = write_enable_i;
+        MEPC_ADDR: mepc_en = write_enable_i | trap_i;
+        MCAUSE_ADDR: mcause_en = write_enable_i | trap_i;
+      endcase // case (addr_i)
+   end
+
+   always @ (posedge clk_i or posedge rst_i) begin
+      if (mie_en) begin
+         if (rst_i) mie_q = 'd0;
+         else mie_q = reg_data;
+      end
+      if (mtvec_en) begin
+         if (rst_i) mtvec_q = 'd0;
+         else mtvec_q = reg_data;
+      end
+      if (mscratch_en) begin
+         if (rst_i) mscratch_q = 'd0;
+         else mscratch_q = reg_data;
+      end
+      if (mepc_en) begin
+         if (rst_i) mepc_q = 'd0;
+         else mepc_q = trap_i ? reg_data : pc_i;
+      end
+      if (mcause_en) begin
+         if (rst_i) mcause_q = 'd0;
+         else mcause_q = trap_i ? reg_data : mcause_i;
+      end
+   end
+
+   always @ (*) begin
+      case (addr_i)
+        MIE_ADDR: csr = mie_q;
+        MTVEC_ADDR: csr = mtvec_q;
+        MSCRATCH_ADDR: csr = mscratch_q;
+        MEPC_ADDR: csr = mepc_q;
+        MCAUSE_ADDR: csr = mcause_q;
+        default: csr = 'd0;
+      endcase // case (addr_i)
+   end
 
 endmodule // csr_constroller
