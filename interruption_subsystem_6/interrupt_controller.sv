56332f847bd05294f862cf3d10bafd3855783491
versadinary <170194434+versadinary@users.noreply.github.com>
2025-10-20 08:28:05 +0300

    irq ready

--- interruption_subsystem_6/interrupt_controller.sv
+++ interruption_subsystem_6/interrupt_controller.sv
@@ -29,6 +29,7 @@ module interrupt_controller(
 
    assign exc_h_d = exc_rst;
    assign irq_h_d = irq_rst;
+
    assign irq_out = ~(exc_set | irq_h_q) & (irq_req_i & mie_i);
    assign irq_ret_o = mret_i & ~exc_set;
    assign irq_o = irq_out;
