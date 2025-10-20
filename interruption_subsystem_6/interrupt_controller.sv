9c809c82b68c8a53fa88ea5d034ff88b24c07c34
versadinary <170194434+versadinary@users.noreply.github.com>
2025-10-20 08:12:03 +0300

    add irq controller

--- interruption_subsystem_6/interrupt_controller.sv
+++ interruption_subsystem_6/interrupt_controller.sv
@@ -12,8 +12,37 @@ module interrupt_controller(
                             );
 
 
+   logic                                        exc_h_d;
+   logic                                        irq_h_d;
+   logic                                        exc_h_q;
+   logic                                        irq_h_q;
+   logic                                        irq_out;
+   logic                                        exc_set;
+   logic                                        exc_rst;
+   logic                                        irq_set;
+   logic                                        irq_rst;
 
+   assign exc_set = exc_h_q | exception_i;
+   assign exc_rst = exc_set & ~mret_i;
+   assign irq_set = irq_h_q | irq_out;
+   assign irq_rst = irq_set & ~(mret_i & ~exc_set);
 
+   assign exc_h_d = exc_rst;
+   assign irq_h_d = irq_rst;
+   assign irq_o = ~(exc_set | irq_q) & (irq_req_i & mret_i);
+   assign irq_ret_o = mret_i & ~exc_set;
+   assign irq_o = irq_out;
+   assign irq_cause_o = 32'h8000_0010;
 
+   always @ (posedge clk) begin
+      if (rst) begin
+         exc_h_q <= 1'd0;
+         irq_h_q <= 1'd0;
+      end
+      else begin
+         exc_h_q <= exc_h_d;
+         irq_h_q <= irq_h_d;
+      end
+   end
 
 endmodule
