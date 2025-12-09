07ddd5740c45547eeff383fe6d313077df9bafce
dosttiz <tfkpcalc@gmail.com>
2025-12-09 21:02:37 +0300

    5

--- programming_8/build.sh
+++ programming_8/build.sh
@@ -4,8 +4,8 @@
 
 ~/riscv_cc/bin/riscv-none-elf-gcc -march=rv32i_zicsr -mabi=ilp32 -Wl,--gc-sections -nostartfiles -T linker_script.ld startup.o main.o -o result.elf &&
 
-~/riscv_cc/bin/riscv-none-elf-objcopy -O verilog -j .text result.elf init_instr.mem && 
+~/riscv_cc/bin/riscv-none-elf-objcopy -O verilog --verilog-data-width=4 -j .text result.elf init_instr.mem && 
 
-~/riscv_cc/bin/riscv-none-elf-objcopy -O verilog -j .data -j .bss -j .sdata result.elf init_data.mem &&
+~/riscv_cc/bin/riscv-none-elf-objcopy -O verilog --verilog-data-width=4 -j .data -j .bss -j .sdata result.elf init_data.mem &&
 
 ~/riscv_cc/bin/riscv-none-elf-objdump -D result.elf > disasm.S
