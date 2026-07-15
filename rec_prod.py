034630fa2da43519a3b9b97671686777736e9efb
dosttiz <>
2026-07-15 15:05:45 +0300

    cleanup

--- rec_prod.py
+++
@@ -1,9 +0,0 @@
-def product(a, b):
-    res = 0
-    one = 1
-    if (b == 0): return res
-    check = b & one
-    if (check): res += a
-    return res + product(a << one, b >> one)
-
-print(product(*map(int, input().split())))
--- script.py
+++
@@ -1,9 +0,0 @@
-def first_operand(num):
-    return hex(((num < 0) << 23) + num * 2)[2::]
-
-def second_operand(num):
-    return hex(((num < 0) << 16) + num)[2::]
-
-a, sw = int(input("a: ")), int(input("sw: "))
-print("a:", first_operand(a))
-print("sw:", second_operand(sw))
