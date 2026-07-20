a0c1eae0d35aa5a06720222cc29f2e2a95d2f2fb
dosttiz <>
2026-07-20 13:18:38 +0300

    test program for VGA

--- test_programs/test_vga/main.c
+++ test_programs/test_vga/main.c
@@ -10,27 +10,44 @@ void int_handler() {
     ps2_scan = ps2_ptr->unread_data ? ps2_ptr->scan_code : 0;
     switch (ps2_scan) {
         case 0x1D:
+            vga.char_map[pos] = ' '; 
             pos -= COLS;
+            vga.char_map[pos] = 'X'; 
+            vga.color_map[pos] = 0xf0;
             break;
         case 0x1C:
+            vga.char_map[pos] = ' '; 
             pos -= 1;
+            vga.char_map[pos] = 'X'; 
+            vga.color_map[pos] = 0xf0;
             break;
         case 0x1B:
+            vga.char_map[pos] = ' '; 
             pos += COLS;
+            vga.char_map[pos] = 'X'; 
+            vga.color_map[pos] = 0xf0;
             break;
         case 0x23:
+            vga.char_map[pos] = ' '; 
             pos += 1;
+            vga.char_map[pos] = 'X'; 
+            vga.color_map[pos] = 0xf0;
             break;
+        case 0x5A:
+            vga.color_map[pos] = 0xff;
+            break;
+        case 0x76:
+            vga.color_map[pos] = 0x00;
+            break;
+
         }
-    vga.color_map[pos] = 0xff;
-    vga.color_map[pos] = 0xcc;
 
 }
 
 void clear_canvas() {
     for (int i = 0; i < ROWS; i++) {
         for (int k = 0; k < COLS; k++) {
-            vga.char_map[i * COLS + k] = 0;
+            vga.char_map[i * COLS + k] = ' ';
             vga.color_map[i * COLS + k] = 0;
         }
     }
