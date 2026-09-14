ebca078b0f9b795464001b4ceedcc01716de49b2
nikitakol442 <nikitakol442@gmail.com>
2026-09-14 12:27:30 +0300

    periph

--- uart_hal_utest/sw/uart_unit_testing/platform/platform.c
+++ uart_hal_utest/sw/uart_unit_testing/platform/platform.c
@@ -1,1 +1,21 @@
-void config_periph();
+#include "peripherals_addr.h"
+#include "unit.h"
+
+void config_uart_rx()
+{
+    
+
+}
+
+
+void config_uart_rx()
+{
+    
+
+}
+
+void send_char(virtual_uart *uart_inst, char data_to_send)
+{
+    
+
+}
